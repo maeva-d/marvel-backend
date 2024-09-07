cada3b4b97240ae09e85f28d948f74c2e37ceee5
Maeva DTC <delruemaeva@orange.fr>
2024-09-07 23:30:49 +0200

    handled pagination in back

--- routes/characters.js
+++ routes/characters.js
@@ -12,12 +12,13 @@ router.get("/characters", async (req, res) => {
       // console.log(req.query)
       // On met des paramètres par défaut grâce au || !! si une des valeurs est falsy alors c'est l'autre qui sera prise en compte
       const limit = req.query.limit || 100;
-      const skip = req.query.skip || 0;
+      const page = req.query.page || 1;
+      const skip = (page - 1) * limit;
       const name = req.query.name || "";
 
       // Je veux récupérer des données pour les mettre dans mon back...
       const response = await axios.get(
-        `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&limit=${limit}&skip=${skip}&name=${name}`
+        `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&limit=${limit}&skip=${skip}&name=${name}&page=${page}`
       );
       const data = response.data;
       // ... qui pourront être renvoyées quand le client interrogera la route voulue
