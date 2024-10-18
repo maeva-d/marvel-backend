c09c919ccce6186bda0c7ecd9f8164bb6f19c4d2
Maeva D <delruemaeva@orange.fr>
2024-10-18 15:38:39 +0200

    fix apikey condition  showing in comics and comics/:characaterID endpoints

--- routes/comics.js
+++ routes/comics.js
@@ -8,24 +8,21 @@ require("dotenv").config();
 // Il faut tester avec localhost sur postman les routes pour voir si les requêtes fonctionnent
 router.get("/comics", async (req, res) => {
   try {
-    if (req.query.apiKey === process.env.API_KEY) {
-      // On mets des paramètres par défaut !!
-      const limit = req.query.limit || 100;
-      const page = req.query.page || 1;
-      const skip = (page - 1) * limit;
-      const title = req.query.title || "";
+    const limit = req.query.limit || 100;
+    const page = req.query.page || 1;
+    const skip = (page - 1) * limit;
+    const title = req.query.title || "";
 
-      const response = await axios.get(
-        `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}&limit=${limit}&skip=${skip}&title=${title}&page=${page}`
-      );
-      // console.log(response.data); OK !
-      const data = response.data;
-      return res.status(200).json(data);
-    } else {
-      return res.status(401).json("Access denied: you need a valid API key.");
-    }
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}&limit=${limit}&skip=${skip}&title=${title}&page=${page}`
+    );
+    return res.status(200).json(response.data);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    if (error.message === "Your apiKey is not valid") {
+      return res.status(400).json({ message: "apiKey is not valid." });
+    } else {
+      return res.status(500).json({ message: error.message });
+    }
   }
 });
 
@@ -38,18 +35,21 @@ router.get("/comics/:characterId", async (req, res) => {
     const skip = (page - 1) * limit;
     // console.log(req.query);
     // console.log(req.params);
-    if (req.query.apiKey === process.env.API_KEY) {
-      const response = await axios.get(
-        `https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.characterId}?apiKey=${process.env.API_KEY}&page=${page}`
-      );
-      const data = response.data;
-      // console.log("data =>", data);
-      return res.status(200).json(data);
-    } else {
-      return res.status(401).json("Access denied: you need a valid API key.");
-    }
+    // if (req.query.apiKey === process.env.API_KEY) {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.characterId}?apiKey=${process.env.API_KEY}&page=${page}`
+    );
+    const data = response.data;
+    return res.status(200).json(data);
+    // } else {
+    //   return res.status(401).json("Access denied: you need a valid API key.");
+    // }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    if (error.message === "Your apiKey is not valid") {
+      return res.status(400).json({ message: "apiKey is not valid." });
+    } else {
+      return res.status(500).json({ message: error.message });
+    }
   }
 });
 
