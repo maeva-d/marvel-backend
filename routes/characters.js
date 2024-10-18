8110c048bf8bc44708b733350fdc2e34f6ffeffb
Maeva D <delruemaeva@orange.fr>
2024-10-18 15:40:52 +0200

    fix character route condition

--- routes/characters.js
+++ routes/characters.js
@@ -8,24 +8,24 @@ require("dotenv").config();
 
 router.get("/characters", async (req, res) => {
   try {
-    if (req.query.apiKey === process.env.API_KEY) {
-      // console.log(req.query)
-      // On met des paramètres par défaut grâce au || !! si une des valeurs est falsy alors c'est l'autre qui sera prise en compte
-      const limit = req.query.limit || 100;
-      const page = req.query.page || 1;
-      const skip = (page - 1) * limit;
-      const name = req.query.name || "";
+    // if (req.query.apiKey === process.env.API_KEY) {
+    // console.log(req.query)
+    // On met des paramètres par défaut grâce au || !! si une des valeurs est falsy alors c'est l'autre qui sera prise en compte
+    const limit = req.query.limit || 100;
+    const page = req.query.page || 1;
+    const skip = (page - 1) * limit;
+    const name = req.query.name || "";
 
-      // Je veux récupérer des données pour les mettre dans mon back...
-      const response = await axios.get(
-        `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&limit=${limit}&skip=${skip}&name=${name}&page=${page}`
-      );
-      const data = response.data;
-      // ... qui pourront être renvoyées quand le client interrogera la route voulue
-      return res.status(200).json(data);
-    } else {
-      return res.status(401).json("Access denied: you need a valid API key.");
-    }
+    // Je veux récupérer des données pour les mettre dans mon back...
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&limit=${limit}&skip=${skip}&name=${name}&page=${page}`
+    );
+    const data = response.data;
+    // ... qui pourront être renvoyées quand le client interrogera la route voulue
+    return res.status(200).json(data);
+    // } else {
+    //   return res.status(401).json("Access denied: you need a valid API key.");
+    // }
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
